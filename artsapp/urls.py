7066a7cea41ab288faf6d52ae5e3ae3c544d84ee
Tarun <tarun.cosmic@gmail.com>
2023-01-02 19:46:48 +0530

    added login system

--- artsapp/urls.py
+++ artsapp/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('aboutme/', views.about, name='about'),
     path('contactme/', views.contact, name='contact'),
     path('art/<int:pk>', views.details, name='art_details'),
+    path('signinpage/', views.openSignIn, name='opensignin'),
+    path('signuppage/', views.openSignUp, name='opensignup'),
+    path('signin', views.signin, name='signin'),
+    path('signup', views.signup, name='signup'),
+    path('signout/', views.signout, name='signout')
 ]
--- artsapp/views.py
+++ artsapp/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 from .models import PostModel
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 # a list view is a *LIST* of all the objects
 # Detail View is something like the data about one Object
 
@@ -24,10 +27,6 @@ def portfolio(request):
     return render(request, template_name='portfolio.html')
 
 
-def login(request):
-    return render(request, template_name='login.html')
-
-
 def create(request):
     posts = PostModel.objects.all()
     context = {"posts": posts}
@@ -38,3 +37,51 @@ def details(request, pk):
     posts = PostModel.objects.filter(id=pk)
     context = {"posts": posts}
     return render(request, template_name='art_details.html', context=context)
+
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        newuser = User.objects.create_user(
+            username=username, email=email, password=password)
+        newuser.save()
+
+        messages.success(
+            request, 'Congratulations! A New user has been saved.')
+
+    return redirect('homepage')
+
+
+def signin(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(
+            request=request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Successfully logged in!')
+        else:
+            messages.error(request, 'Wrong Credentials!')
+        return redirect('homepage')  # might need to change to ''
+    # return render(request, template_name='home.html')
+
+
+def openSignUp(request):
+    return render(request=request, template_name='signup.html')
+
+
+def openSignIn(request):
+    return render(request, template_name='signin.html')
+
+
+def signout(request):
+    logout(request)
+    messages.success(request, 'Logged out!')
+    return redirect('homepage')
